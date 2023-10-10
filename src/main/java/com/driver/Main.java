df5024b25ad8b78ce7e558ad325421eafb4e4cdd
Mausam Raj <128462297+Mausam1815@users.noreply.github.com>
2023-10-10 21:11:21 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
+  RWOnly rwonlyObj = new RWOnly();
   
+  // rwonlyObj.name = "Mausam";
+  // System.out.println(rwonlyObj.name);
+  
+  // ERROR : Exception in thread "main" java.lang.Error: Unresolved compilation problems: 
+  // The field RWOnly.name is not visible
+  // The field RWOnly.name is not visible
+  rwonlyObj.setName("Mausam");
+  System.out.println(rwonlyObj.getName());
 }
